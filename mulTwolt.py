b960a07e0194ddd4ebf4597bc617f0167098eff9
toure1802 <jeunessefcm@gmail.com>
2020-07-14 06:31:38 -0400

    mulTwolt.py

--- mulTwolt.py
+++ mulTwolt.py
@@ -1,7 +1,11 @@
+def main():
+    mul()
+
 import sys
 def mul(x,y):
     return x*y
 if __name__=="__main__":
+    main()
 	print(sys.argv)    
 	if(len(sys.argv)==3):
 		x = int(sys.argv[1])
@@ -11,16 +15,9 @@ if __name__=="__main__":
 		x = int(sys.argv[1])
 		y = int(input("Entrez la troisieme valeur svp ? "))
 		print(mul(x,y))
-	if(len(sys.argv)==2):
-		print("error")
 	elif(len(sys.argv)==1):
 		x = int(input("Entrez la deuxieme la valeur svp ? "))
 		y = int(input("Entrez la troixieme valeur svp ? "))
 		print(mul(x,y))
 	else:
 		print("Desoler erreur"))
-		print("error")
-        else:
-                x = int(sys.argv[1])
-                y = int(sys.argv[2])
-                print(mul(x,y)i)
